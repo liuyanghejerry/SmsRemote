2123141dad6562edfc0b855c30246462b90e1f25
Yanghe Liu <liuyanghejerry@126.com>
2013-12-10 14:50:56 +0800

    change to preference style

--- src/com/liuyanghejerry/smsremote/MainActivity.java
+++ src/com/liuyanghejerry/smsremote/MainActivity.java
@@ -1,27 +1,34 @@
 package com.liuyanghejerry.smsremote;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
+import android.preference.PreferenceActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PreferenceActivity {
 
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        addPreferencesFromResource(R.xml.preference);
+//        setContentView(R.layout.activity_main);
     }
     
-    public void saveSettings(View view) {
-    	EditText sender = (EditText) findViewById(R.id.phoneNumber);
-    	String phoneNumber = sender.getText().toString();
-    	EditText post_url = (EditText) findViewById(R.id.postUrl);
-    	String postUrl = post_url.getText().toString();
-    	CheckBox enabled = (CheckBox) findViewById(R.id.isEnabled);
-    	boolean isEnabled = enabled.isChecked();
-    	System.out.println(phoneNumber+postUrl);
-    }
+    // for higher API level, started from Android 3.0
+//    @Override
+//    protected void onCreate(final Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+//    }
+//
+//    public static class MyPreferenceFragment extends PreferenceFragment
+//    {
+//        @Override
+//        public void onCreate(final Bundle savedInstanceState)
+//        {
+//            super.onCreate(savedInstanceState);
+//            addPreferencesFromResource(R.xml.preference);
+//        }
+//    }
 }
  
